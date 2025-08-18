54e4b28bdfa60857c812f8314ace952ff09623ce
Archisman Panigrahi <apandada1@gmail.com>
2025-08-18 19:54:16 -0400

    update version

--- bump-version.sh
+++ bump-version.sh
@@ -44,10 +44,29 @@ else
     echo "$ENTRY" > "$CHANGELOG"
 fi
 
-SPECFILE="screen-orientation-manager.spec"
+SPECFILE="fedora-package.spec"
 if [ -f "$SPECFILE" ]; then
     sed -i -E "s/^(Version:[[:space:]]*)[0-9.]+/\1$NEW_VERSION/" "$SPECFILE"
     echo "Version updated to $NEW_VERSION in $PYFILE, $SPECFILE and changelog updated."
 else
     echo "Version updated to $NEW_VERSION in $PYFILE and changelog updated. (Spec file not found)"
+fi
+
+METAINFO="io.github.archisman_panigrahi.screen-orientation-manager.metainfo.xml"
+
+# Update metainfo.xml with new release entry
+if [ -f "$METAINFO" ]; then
+    RELEASE_DATE=$(date +%Y-%m-%d)
+    RELEASE_ENTRY="    <release version=\"$NEW_VERSION\" date=\"$RELEASE_DATE\">
+      <description>
+        $CHANGELOG_MSG
+      </description>
+    </release>"
+    # Insert after the opening <releases> tag
+    awk -v entry="$RELEASE_ENTRY" '
+        /<releases>/ && !x {print; print entry; x=1; next} 1
+    ' "$METAINFO" > "${METAINFO}.tmp" && mv "${METAINFO}.tmp" "$METAINFO"
+    echo "Metainfo updated with new release entry."
+else
+    echo "Metainfo file not found, skipping."
 fi
--- fedora-package.spec
+++ fedora-package.spec
@@ -1,5 +1,5 @@
 Name:           screen-orientation-manager
-Version:        1.4
+Version:        1.4.1
 Release:        1%{?dist}
 Summary:        Screen Orientation Manager for X11
 
